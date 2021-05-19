3d42fc919c03d9371922a605810b22558d1c07c5
ganeshkumar269 <mganeshkumar269@gmail.com>
2021-05-19 16:30:52 +0530

    added return after sending response

--- src/handleCrawl.js
+++ src/handleCrawl.js
@@ -1,11 +1,18 @@
 const validateUrl = require('../util/validateUrl')
 
 const handleCrawl = (req,res)=>{
+
     const id_token = req.token
     const url = req.websiteUrl
-    if(!validateUrl(url)) res.status(400).send({"status":400})
+
+    if(!validateUrl(url)) {
+        res.status(400).send({"status":400})
+        return
+    }
+
     let tempList = ["/about","/home","/contact","/privacy","/random"]
     res.send({status:200,urls:tempList})
+
 }
 
 module.exports = handleCrawl
