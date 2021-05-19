1ae48b8a906fdd29360d2b6a5bee0687d06e14ea
ganeshkumar269 <mganeshkumar269@gmail.com>
2021-05-19 16:34:57 +0530

    added return statement after response

--- middlewares/verifyTokenMiddleware.js
+++ middlewares/verifyTokenMiddleware.js
@@ -2,7 +2,10 @@ const verifyIdToken = require('../util/verifyIdToken')
 
 const verifyIdTokenMiddleware = (req,res,next) =>{
     let id_token = req.header('Authorization')
-    if(!id_token) res.status(400).json({status:400, message:"Auth Token Not Found"})
+    if(!id_token) {
+        res.status(400).json({status:400, message:"Auth Token Not Found"})
+        return
+    }
     if(verifyIdToken(id_token)){
         res.token = id_token;
         next()
