d6aeff1754aa555647c5510bbc448771b4e93934
ganeshkumar269 <mganeshkumar269@gmail.com>
2021-05-19 16:06:11 +0530

    addex next parameter

--- middlewares/verifyTokenMiddleware.js
+++ middlewares/verifyTokenMiddleware.js
@@ -1,6 +1,6 @@
 const verifyIdToken = require('../util/verifyIdToken')
 
-const verifyIdTokenMiddleware = (req,res) =>{
+const verifyIdTokenMiddleware = (req,res,next) =>{
     let id_token = req.header('Authorization')
     if(!id_token) res.json({status:400, message:"Auth Token Not Found"}).status(400)
     if(verifyIdToken(id_token)){
