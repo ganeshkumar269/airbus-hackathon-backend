4e7f55500b6fa0d831c00339fe8d55e10f470f60
Srinjan Ghosh <srinjanghosh11@gmail.com>
2021-05-21 12:06:23 +0530

    updated routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const verifyTokenMiddleware = require('../middlewares/verifyTokenMiddleware.js')
 
 const {registerUser, loginUser, getKeywords, addKeywords} = require('../controllers/admin.js')
 
@@ -10,7 +11,8 @@ router.post('/login', loginUser)
 
 router.post('/register',registerUser) 
 
-router.get('/keywords', getKeywords)
-router.post('/keywords', addKeywords)
+router.get('/keywords',verifyTokenMiddleware, getKeywords)
+router.post('/keywords',verifyTokenMiddleware, addKeywords)
+
 
 module.exports = router
