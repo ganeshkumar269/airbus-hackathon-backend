113ef3a6484aeaed7941266e91cd81899a368898
ganeshkumar269 <mganeshkumar269@gmail.com>
2021-05-21 15:31:27 +0530

    updated routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,54 +1,76 @@
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {v4:uuidv4} = require('uuid')
 
 const logger = require('../util/loggermodule')
 const dbConn = require('../db/getDbConn').getDbConn;
 
 const loginUser = async function (req, res) {
     const email = req.body.email
-    const client = await dbConn();
-    const database = client.db("ahd")
-    const users = database.collection("users")
+    const password = req.body.password
+    if(email==null || password==null){
+        logger.error("No login data")
+        res.status(404).send({"msg": "Data is Missing"})
+    }else {        
+        const client = await dbConn();
+        const database = client.db("ahd")
+        const users = database.collection("users")
 
-    const user = await users.findOne({email: email})
-    if(user!=null){
-        const passwordMatch = await bcrypt.compare(req.body.password, user.password)
+        const user = await users.findOne({email: email})
+        if(user!=null){
+            const passwordMatch = await bcrypt.compare(req.body.password, user.password)
 
-        if(passwordMatch){
-            res.status(200).json({"msg":"User logged in"});
-        }else {
+            if(passwordMatch){
+                logger.info("User logged In")
+                let token = jwt.sign({user:user}, "secret")
+                res.cookie('token',token, { maxAge: 900000, httpOnly: true });
+                res.status(200).json({"token":token});
+                
+            }else {
+                res.status(404).json({"msg": "User name or password is wrong"})
+            }
+        }else{
             res.status(404).json({"msg": "User name or password is wrong"})
         }
-    }else{
-        res.status(404).json({"msg": "User name or password is wrong"})
     }
 
 }
 
 const registerUser = async function(req, res) {
-    isPresent = await checkIfUserAlreadyPresent(req.body.email)
-    if(isPresent){
-        res.status(404).json({"msg": "User already present"})
-    }else {
-        try {
-            const hashedPassword = await bcrypt.hash(req.body.password, 9)
-            const user = {
-                email: req.body.email,
-                password: hashedPassword
+    const email = req.body.email
+    const password = req.body.password
+    if(email==null || password==null){
+        logger.error("No login data")
+        res.status(404).send({"msg": "Data is Missing"})
+    } else {
+        isPresent = await checkIfUserAlreadyPresent(req.body.email)
+        if(isPresent){
+            logger.info("User already present")
+            res.status(404).json({"msg": "User already present"})
+        }else {
+            try {
+                const hashedPassword = await bcrypt.hash(req.body.password, 9)
+                const user = {
+                    email: req.body.email,
+                    password: hashedPassword,
+                    website_id: uuidv4() 
+                }
+    
+                const client = await dbConn();
+                const database = client.db("ahd")
+                const users = database.collection("users")
+    
+                const result = await users.insertOne(user)
+    
+                if(result){
+                    res.status(200).json({"msg":"User created Successfully"})
+                }else {
+                    res.status(500).json({"error": err.message})
+                }
+            } catch (error) {
+                logger.error(error.message)
             }
 
-            const client = await dbConn();
-            const database = client.db("ahd")
-            const users = database.collection("users")
-
-            const result = await users.insertOne(user)
-
-            if(result){
-                res.status(200).json({"msg":"User created Successfully"})
-            }else {
-                res.status(500).json({"error": err.message})
-            }
-        } catch (error) {
-            console.log(error)
         }
     }
 }
@@ -58,34 +80,64 @@ const getFeedbacks = function(req, res) {
 }
 
 const getKeywords = async function(req, res) {
-    const client = await dbConn();
-    const database = client.db("ahd")
-    const keywords = database.collection("website_keywords")
-
     const websiteId = req.body.website_id;
+    if(websiteId == null){
+        logger.error("website_id not present")
+        res.send(403)
+    }
+    else {
+        const client = await dbConn();
+        const database = client.db("ahd")
+        const keywords = database.collection("website_keywords")
 
-    const keywordsData = await keywords.findOne({website_id: websiteId})
-    const keyword_arr = keywordsData.keywords
+        const keywordsData = await keywords.findOne({website_id: websiteId})
 
-    res.status(200).send(keyword_arr)
+        if(keywordsData){
+            const keyword_arr = keywordsData.keywords
+            res.status(200).send(keyword_arr)
+        } else {
+            logger.info("website id donot match")
+            res.status(404).send({"msg":"website id donot match"})
+        }
+    }
 }
 
 const addKeywords = async function(req, res) {
-    const client = await dbConn();
-    const database = client.db("ahd")
-    const keywordCollection = database.collection("website_keywords")
-
     const websiteId = req.body.website_id;
     const keywords = req.body.keywords;
+    if(websiteId == null || keywords==null){
+        logger.error("website_id or keywords not present")
+        res.send(403)
+    } else {
+        try {
+            const client = await dbConn();
+            const database = client.db("ahd")
+            const keywordCollection = database.collection("website_keywords")
 
-    const result = await keywordCollection.updateOne(
-                        {website_id: websiteId}, {$addToSet: { keywords: {$each:keywords}}}
-                        )
+            const keywordsData = await keywordCollection.findOne({website_id: websiteId})
 
-    if(result)
-        res.status(200).json({"msg" : "keywords added"})
-    else
-        res.status(404)
+            let result
+            if(keywordsData){
+                result = await keywordCollection.updateOne(
+                    {website_id: websiteId}, {$addToSet: { keywords: {$each:keywords}}}
+                    )
+                
+            } else {
+                result = await keywordCollection.insertOne(
+                    {website_id:websiteId, keywords:keywords}
+                    )
+            }
+        
+            if(result)
+                res.status(200).json({"msg" : "keywords added"})
+            else
+                res.send(404)
+        } catch(error) {
+            logger.error(error.message)
+            res.status(500).send({"msg":error.message})
+        }
+    }
+
 }
 
 async function checkIfUserAlreadyPresent(email) {
--- controllers/widget.js
+++ controllers/widget.js
@@ -29,7 +29,53 @@ const feedback = function(req, res) {
         })
 }
 
+const addBugReport = async function(req, res) {
+    const websiteId = req.body.website_id;
+    let bugReport = req.body.bugReport;
+    if(websiteId == null || bugReport == null){
+        logger.error("website_id or bug report not present")
+        res.send(403)
+    } else {
+        try {
+            const client = await dbConn();
+            const database = client.db("ahd")
+            const collection = database.collection("bugReports")
+
+            const data = await collection.findOne({website_id: websiteId})
+            bugReport = {id: Date.now(), ...bugReport}
+            let result
+            if(data){
+                result = await collection.updateOne(
+                    {website_id: websiteId}, {$addToSet: { bugReport: bugReport}}
+                    )
+                
+            } else {
+                result = await collection.insertOne(
+                    {website_id:websiteId, bugReport:[bugReport]}
+                    )
+            }
+        
+            if(result)
+                res.status(200).json({"msg" : "bug report added"})
+            else
+                res.send(404)
+        } catch(error) {
+            logger.error(error.message)
+            res.status(500).send({"msg":error.message})
+        }
+    }
+}
+
+const userLogin = async function(req, res) {
+
+}
+
+const userRegister = async function(req, res) {
+    
+}
+
 module.exports = {
     // handleSearch,
     feedback,
+    addBugReport,
 }
